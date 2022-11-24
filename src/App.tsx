afc8521abbccddffaad62e6303c9b69f3e0156f6
daniellerodrigues <daniellerodrigues-14@live.com>
2022-11-24 10:53:49 -0300

    add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Title } from './Componentes/title/title'
 import { Text } from './Componentes/Text/Text'
+import Navbar from './Componentes/Navbar/Navbar';
 
 function App() {
   const nome =  'Danielle';
 
   return (
     <>
-    
+    <Navbar/>
    <Title nome={nome} />
    <Text />
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: purple[500],
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ThemeProvider  theme={theme}>
     <App />
+   </ThemeProvider>
   </React.StrictMode>
 );
 
