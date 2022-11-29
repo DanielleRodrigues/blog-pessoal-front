82025c141f55498d5ff6e3be25ba2858b3804773
daniellerodrigues <daniellerodrigues-14@live.com>
2022-11-29 14:21:45 -0300

    add hooks

--- src/paginas/Home/Home.tsx
+++ src/paginas/Home/Home.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 
 function Home() {
+  const [num, setNum] = useState(0);
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <div>vc clicou {num} vezes </div>
+      <button onClick={()=> setNum(num + 1)}>click here</button>
+    </>
+  );
 }
 
 export {Home}
