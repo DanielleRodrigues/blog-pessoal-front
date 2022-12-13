1a1ac8a863e21ae00b833ed357f7b1df052d3937
Danielle Rodrigues <daniellerodrigues-14@live.com>
2022-12-12 23:42:30 -0300

    rebuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,63 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Componentes/estaticos/Navbar/Navbar';
-import Footer from './Componentes/estaticos/Footer/Footer';
-import Home from './paginas/Home/Home';
-import './App.css';
-import Login from './paginas/login/Login';
+import Navbar from './components/estaticos/Navbar/Navbar';
+import Footer from './components/estaticos/Footer/Footer';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import Home from './paginas/Home/Home';
+import Login from './paginas/Login/Login';
+import './App.css';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletaPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import {Provider} from 'react-redux';
+import store from './store/store';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
-    <Router>
+    <Provider store={store}>
+      <ToastContainer />
+      <Router>
       <Navbar />
-      <Routes>
+      <div style={{ minHeight: '100vh' }}>
+        <Routes>
 
-        <Route path="/" element={<Login  />} />
+          <Route path="/" element={<Login />} />
 
-        <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
 
-        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
 
-      </Routes>
+          <Route path="/temas" element={<ListaTema />} />
+
+          <Route path="/posts" element={<ListaPostagem />} />
+
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+          <Route path="/formularioTema" element={<CadastroTema />} />
+
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+
+        </Routes>
+      </div>
       <Footer />
-    </Router>
 
+    </Router>
+    </Provider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@material-ui/core';
-import { createTheme } from '@material-ui/core/styles';
-import { purple } from '@material-ui/core/colors';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      // Purple and green play nicely together.
-      main: purple[500],
-    },
-    secondary: {
-      // This is green.A700 as hex.
-      main: '#11cb5f',
-    },
-  },
-});
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider  theme={theme}>
     <App />
-   </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
